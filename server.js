a1e2ac6fc6f7c2b37b72f1b7991915ac011d7562
Vincent Taing <taing.vincent@gmail.com>
2017-02-07 17:06:19 -0800

    update server.js

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
+// @flow
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const flowCoverage = require('./flow-coverage');
+const exec = require("child_process").exec;
+
 
 app.use(cors());
 app.use(bodyParser.json());
 
 
-flowCoverage();
+function printError(error, stdout) {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(stdout);
+}
 
 app.get("/github/payload", () => {
+  exec("git pull -f origin master", printError);
+  flowCoverage();
+});
 
+
+app.listen(1337, () => {
+  console.log('Example app listening on port 8080!');
 });
